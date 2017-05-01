18272de4bad360a8c14b2225cfb4286d9582e037
Josep Ribes <esderoser@gmail.com>
2017-05-01 16:33:52 +0200

    Initial Project Version

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("--------Prueba JDBC----------------");
+        compruebaJDBC();
+
+        Connection conexion;    
+        Statement sentencia;
+        ResultSet resultado;
+        try{
+            conexion = DriverManager.getConnection( "jdbc:mysql://localhost/biblioteca","root","" );
+            sentencia = conexion.createStatement();
+            resultado = sentencia.executeQuery( "SELECT * FROM vista1" );
+            while( resultado.next()){
+                String titulo = resultado.getString( "Titulo" );
+                String autor = resultado.getString("Autor");
+                System.out.println(titulo+"|"+autor);
+            }
+            conexion.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    
+    
+    }
+    
+    public static void compruebaJDBC(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println("El programa ha encontrado el JDBC");
+    }
+}
+
+
+
+
+
